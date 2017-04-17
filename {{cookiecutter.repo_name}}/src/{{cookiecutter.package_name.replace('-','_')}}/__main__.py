844d058c7d2f9dc588531a689f94e65bd993849c
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2017-04-17 18:57:22 +0200

    Fix pylint warning when click, fix whitelist tox

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name.replace('-','_')}}/__main__.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name.replace('-','_')}}/__main__.py
@@ -9,14 +9,13 @@ Why does this file exist, and why __main__? For more info, read:
 - https://docs.python.org/2/using/cmdline.html#cmdoption-m
 - https://docs.python.org/3/using/cmdline.html#cmdoption-m
 """
-{%- if cookiecutter.command_line_interface|lower == 'plain' %}
 import sys
-{% endif %}
+
 from {{cookiecutter.package_name}}.cli import main
 
 if __name__ == "__main__":
 {%- if cookiecutter.command_line_interface|lower == 'plain' %}
-    sys.exit(main())
+    sys.exit(main(sys.argv))
 {%- else %}
-    main()
+    main(sys.argv)
 {%- endif %}
